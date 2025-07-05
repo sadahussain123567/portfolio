393d4934d69eb80aee05d5a110583d4f1c117ed7
saddam hussain <159890907+sadahussain123567@users.noreply.github.com>
2025-07-05 15:39:53 +0500

    Update page.tsx

--- portfolio-website-using--tailwind-css-main/app/page.tsx
+++ portfolio-website-using--tailwind-css-main/app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from "@/components/Hero";
-
+import About from "@/components/About";
+import Contact from "@/components/Contact";
 export default function Home() {
   return (
     <div>
       <Hero />
+      <About />
+      <Contact />
     </div>
   );
 }
