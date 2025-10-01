38fd915e6635029d913ad699e83ca491a7632988
saddam hussain <159890907+sadahussain123567@users.noreply.github.com>
2025-10-01 00:53:18 -0700

    Update Skills.tsx

--- portfolio-website-using--tailwind-css-main/components/Skills.tsx
+++ portfolio-website-using--tailwind-css-main/components/Skills.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import React from "react";
+
 type Props = {
   src: string;
 };
+
 const Skills = (props: Props) => {
   return (
-    <div>
+    <div className="pt-[80px] sm:pt-[120px]"> 
+      {/* 👆 Mobile: 80px, Desktop: 120px */}
       <ul className="flex items-center justify-between gap-4">
         <li>
-          <Image src={props.src} alt="" width={50} height={50} />
+          <Image src={props.src} alt="skill icon" width={50} height={50} />
         </li>
       </ul>
     </div>
